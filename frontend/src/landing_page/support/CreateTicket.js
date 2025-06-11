4e0d121ae550471cf6b034628b0f12d7ba5e08ed
shubham <shubhamsinghmor2312@gmail.com>
2025-06-11 21:36:53 +0530

    Done

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -2,7 +2,7 @@ import React from "react";
 function CreateTicket() {
   return (
     <div className="contianer">
-      <div className="row   p-5">
+      <div className="row   p-6">
         
         <h1 className="text-muted fs-4 " style={{marginLeft:"210px"}}>
           To create a ticket, select a relevant topic
